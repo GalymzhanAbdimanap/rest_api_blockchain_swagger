54bbff372a75ad64dbd8288a29676a299c3f513b
abdi <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2020-08-05 11:06:28 +0600

    Update restplus.py

--- rest_api/api/restplus.py
+++ rest_api/api/restplus.py
@@ -1,18 +1,31 @@
+#!flask/bin/python
+"""
+Blockchain API
+Api for connecting blockchain technology with a mobile application.
+Blockchain: TRON;
+
+Copyright (c) 2020 IDET.kz
+Written by Galymzhan Abdymanap.
+Version 1.0
+"""
+
 import logging
 import traceback
 
 from flask_restplus import Api
 from rest_api import settings
 
-
+# Create logger
 log = logging.getLogger(__name__)
 
+# Api init
 api = Api(version='1.0', title='Tron Blockchain API',
           description='A simple demonstration of a Flask RestPlus powered API')
 
 
 @api.errorhandler
 def default_error_handler(e):
+    """"""
     message = 'An unhandled exception occurred.'
     log.exception(message)
 
