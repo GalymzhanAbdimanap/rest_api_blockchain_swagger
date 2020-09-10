734f1ac5f86f8aa99eb0d248e88f7b2dfbeee64c
abdi <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2020-09-10 10:53:51 +0600

    Update utils.py

--- rest_api/api/blockchain/utils.py
+++ rest_api/api/blockchain/utils.py
@@ -3,7 +3,6 @@
 Blockchain API
 Api for connecting blockchain technology with a mobile application.
 Blockchain: TRON;
-
 Copyright (c) 2020 IDET.kz
 Written by Galymzhan Abdymanap.
 Version 1.0
@@ -26,10 +25,25 @@ tron = Tron(full_node=full_node,
 
 #--------------------------------------------------------------------------------
 # Smart contract address and default user address on Blockchain
-SMART_CONTRACT_ADDRESS = '41157290966C5D65633276C184D110E4C1DC96C577'
+SMART_CONTRACT_ADDRESS = '419772BA0D80F691959BAC413FAEDBC44C7FEE8536'
 DEFAULT_ADDRESS = '41FCF23797364C955A23B73F711219FBF5564B2C17'
 
 
+TOKEN_CONTRACT = '411576d1a39ace4f0a24fc52b7a17be97904f6a2b8'
+ADDRESS_STORE = '418C7AFBB25E62271699A1254CFAAF4DAA427D1932' # Seller.
+
+TOKEN_CREATOR = "41FCF23797364C955A23B73F711219FBF5564B2C17"
+TOKEN_CREATOR_PRIVATE_KEY = "464d87c77a61a1065e3d21e6e6be9cd3aaeb0ce59724a77c5e86cbeed38bd9b7"
+                              
+#------------------------------------------------------------------------------
+# Addresses of contract which we use.
+#------------------------------------------------------------------------------
+
+# token contract - 411576d1a39ace4f0a24fc52b7a17be97904f6a2b8
+# old marketplace contract - 41157290966C5D65633276C184D110E4C1DC96C577
+# new marketplace contract - TPmzUP412cPUHZXeSqrBJGxZXywoYcEf7c  // 419772BA0D80F691959BAC413FAEDBC44C7FEE8536
+
+#------------------------------------------------------------------------------
 
 
 def getIndexOfGoods():
@@ -95,3 +109,36 @@ def getAddressOfgoods(account_address):
     return decodeA[0]
 
 
+def getTokens_utils(account_address, value=30000000000):
+    """User get tokens on value 300."""
+    e = {'txid':'Error!'} 
+    logsOfError = ''
+    try:
+        trigger = tron.transaction_builder.trigger_smart_contract(contract_address = TOKEN_CONTRACT,
+                                function_selector = 'transfer(address,uint256)', #без пробелов!
+                                fee_limit=1000000000,
+                                call_value=0,
+                                parameters=[{'type':'address','value':account_address},{'type': 'int256','value':value}],
+                                issuer_address=TOKEN_CREATOR
+                                )
+
+        tron.private_key = TOKEN_CREATOR_PRIVATE_KEY
+        transaction = trigger['transaction']
+        signed1_tx = tron.trx.sign(transaction,True,False)
+        e = tron.trx.broadcast(signed1_tx)
+    except Exception as ex:
+        logsOfError = logsOfError + str(ex)
+    return {'txID':e['txid'], 'logs':logsOfError}
+
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
