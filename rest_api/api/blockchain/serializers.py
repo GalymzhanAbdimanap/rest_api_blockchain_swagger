a8d61547a95db3ff1c3a4222977c0744aa6447a2
abdi <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2020-09-10 10:53:03 +0600

    Update serializers.py

--- rest_api/api/blockchain/serializers.py
+++ rest_api/api/blockchain/serializers.py
@@ -3,7 +3,6 @@
 Blockchain API
 Api for connecting blockchain technology with a mobile application.
 Blockchain: TRON;
-
 Copyright (c) 2020 IDET.kz
 Written by Galymzhan Abdymanap.
 Version 1.0
@@ -14,67 +13,85 @@ from rest_api.api.restplus import api
 
 
 getChecks = api.model('getChecks', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
     
 })
 getWallet = api.model('getWallet', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
     
 })
 balanceOfToken = api.model('balanceOfToken', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
     
 })
 balanceOfTrx = api.model('balanceOfTrx', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
     
 })
 purchaseGoods = api.model('purchaseGoods', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain'),
-    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market'),
-    'amounts_goods':fields.List(fields.Integer,readOnly=True, description='Amount of goods for purchase on Blockchain Market'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market.'),
+    'amounts_goods':fields.List(fields.Integer,readOnly=True, description='Amount of goods for purchase on Blockchain Market.'),
 })
 confirmGoods = api.model('confirmGoods', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain'),
-    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market'),
-    'id_check':fields.Integer(readOnly=True, description='ID of check to confirm purchase on Blockchain Market') 
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market.'),
+    'id_check':fields.Integer(readOnly=True, description='ID of check to confirm purchase on Blockchain Market.') 
 })
 saleOfGoods = api.model('saleOfGoods', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain'),
-    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market'),
-    'amounts_goods':fields.List(fields.Integer,readOnly=True, description='Amount of goods for purchase on Blockchain Market'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market.'),
+    'amounts_goods':fields.List(fields.Integer,readOnly=True, description='Amount of goods for purchase on Blockchain Market.'),
 })
 cancelOfPurchase = api.model('cancelOfPurchase', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchainy'),
-    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market'),
-    'id_check':fields.Integer(readOnly=True, description='ID of check to cancel purchase on Blockchain Market') 
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchainy.'),
+    'id_goods':fields.List(fields.Integer,readOnly=True, description='ID of goods on Blockchain Market.'),
+    'id_check':fields.Integer(readOnly=True, description='ID of check to cancel purchase on Blockchain Market.') 
 })
 freeze_balance = api.model('freeze_balance', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain'),
-    'amount':fields.Integer(readOnly=True, description='Amount of TRX for freeze'),
-    'resource':fields.String(readOnly=True, description='ENERGY or BANDWIDTH'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'amount':fields.Integer(readOnly=True, description='Amount of TRX for freeze.'),
+    'resource':fields.String(readOnly=True, description='ENERGY or BANDWIDTH.'),
 
 })
 unfreeze_balance = api.model('unfreeze_balance', {
-    'account_address': fields.String(readOnly=True, description='Account address on Blockchain'),
-    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain'),
-    'resource':fields.String(readOnly=True, description='ENERGY or BANDWIDTH'),
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'resource':fields.String(readOnly=True, description='ENERGY or BANDWIDTH.'),
 
 })
 registration = api.model('registration', {
-    'login': fields.String(readOnly=True, description='Login for mobile app'),
-    'password':fields.String(readOnly=True, description='Password for mobile app'),
+    'login': fields.String(readOnly=True, description='Login for mobile app.'),
+    'password':fields.String(readOnly=True, description='Password for mobile app.'),
       
 })
 oauth = api.model('oauth', {
-    'login': fields.String(readOnly=True, description='Login for mobile app'),
-    'password':fields.String(readOnly=True, description='Password for mobile app'),
+    'login': fields.String(readOnly=True, description='Login for mobile app.'),
+    'password':fields.String(readOnly=True, description='Password for mobile app.'),
     
 })
 
 
+transfer_token = api.model('transfer_token', {
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'value':fields.Integer(readOnly=True, description='The amount of Token for transfer.'),
+
+})
+getToken = api.model('getToken', {
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+})
+createGoods = api.model('createGoods', {
+    'account_address': fields.String(readOnly=True, description='Account address on Blockchain.'),
+    'privateKey':fields.String(readOnly=True, description='Private key of account on Blockchain.'),
+    'title':fields.String(readOnly=True, description='Title for good.'),
+    'startPrice':fields.Integer(readOnly=True, description='Start price for one good.'),
+    'description':fields.String(readOnly=True, description='Description for food.'),
+    'count':fields.Integer(readOnly=True, description='How many pieces for sales.'),
+    'comission':fields.Integer(readOnly=True, description='Comission for one piece.'),
+})
